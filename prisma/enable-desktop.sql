0d7d1e27ed917d03a1770dc22ce379293cae45ed
Lucasp74 <pelutrelucas@gmail.com>
2026-08-17 18:26:12 -0300

    Remove script one-off enable-desktop.sql: contradizia o gating por plano

--- prisma/enable-desktop.sql
+++
@@ -1,3 +0,0 @@
--- Libera o app desktop (flagDesktopSync) para todas as organizações.
--- One-off: rodar em produção uma vez. Não faz parte das migrações.
-UPDATE "Tenant" SET "flagDesktopSync" = true;
